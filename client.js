4229f0c10f0404ff41afa70dd2a385c35631be25
pedro <tecpedrohl@gmail.com>
2022-07-11 13:26:55 -0300

    cliente e servidor conseguem se comunicar e encerrar a comunicação

--- client.js
+++ client.js
@@ -1,8 +1,23 @@
 const net = require('net')
 const readline = require('readline')
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
 const client = new net.Socket()
 client.connect(4000, '127.0.0.1', () => {
     console.log('connectou')
-    client.write('Olá')
+    client.on('data', data => {
+        console.log("Server: " + data)
+    } )
+    rl.addListener('line', line => {
+        client.write(line)
+        if(line === 'end'){
+            console.log("Conexão encerrada")
+            client.end()
+            process.exit()
+        }
+    })
 })
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const net = require('net')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
 const handleConnection = socket => {
     console.log("Alguém sem conectou.")
     socket.on('end', () => {
         console.log('desconectou!')
+        socket.end()
+        process.exit()
     })
     socket.on('data', data => {
-        console.log(data.toString())
+        var msg = data.toString()
+        if(msg === 'end'){
+            socket.end()
+        }
+        console.log("Client: " + msg)
+    })
+    rl.addListener('line', line => {
+        socket.write(line)
+        if(line === 'end'){
+            console.log("Conexão encerrada")
+            socket.end()
+            process.exit()
+        }
     })
 }
 

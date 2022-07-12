08812130ff2c5af28ee4c0254cb70863a5093f1b
pedro <tecpedrohl@gmail.com>
2022-07-12 10:57:03 -0300

    chat multi clients

---
+++ multiple-clients/clients.js
@@ -0,0 +1,22 @@
+const net = require('net')
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const client = new net.Socket()
+client.connect(4000, '127.0.0.1', () => {
+    client.on('data', data => {
+        console.log(data.toString());
+    })
+    rl.addListener('line', line => {
+        client.write(line)
+        // if(line === 'end'){
+        //     console.log("Conexão encerrada")
+        //     client.end()
+        //     process.exit()
+        // }
+    })
+})
--- multiple-clients/server.js
+++ multiple-clients/server.js
@@ -1,2 +1,25 @@
 const net = require('net')
 
+const conns = []
+
+const handleConnection = socket => {
+    socket.id = Math.floor(Math.random() * 1000);
+    socket.write("Informe seu nome");
+    socket.on('data', data => {
+        if(!socket.hasOwnProperty('name')){
+            socket.name = data.toString();
+            conns.push(socket);
+        }else{
+            conns.forEach(conn => {
+                if(conn.id != socket.id){
+                    conn.write(socket.name + ": " + data);
+                }
+            })
+        }
+
+    })
+    // console.log(socket);
+}
+
+const server = net.createServer(handleConnection)
+server.listen(4000, '127.0.0.1')

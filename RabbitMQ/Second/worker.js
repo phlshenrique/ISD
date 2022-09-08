b8f439362482c8dbdc47adc9869f456944d7e417
phlshenrique <phls73376@gmail.com>
2022-09-08 20:49:26 -0300

    Second activity

---
+++ RabbitMQ/Second/worker.js
@@ -0,0 +1,29 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'task_queue';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+
+        channel.consume(queue, function (msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function () {
+                console.log(" [x] Done");
+            }, secs * 1000);
+        }, {
+            noAck: true
+        });
+    });
+});

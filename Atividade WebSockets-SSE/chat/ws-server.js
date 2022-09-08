584c2567692486e72aa31c9072c8ec23d854e8f1
phlshenrique <phls73376@gmail.com>
2022-09-08 13:47:21 -0300

    Storing messages and server-sent event

--- Atividade WebSockets-SSE/chat/ws-server.js
+++ Atividade WebSockets-SSE/chat/ws-server.js
@@ -1,22 +1,48 @@
 var WebSocketServer = require('ws').Server;
+var http = require("http");
+http.createServer(function (req, res) {
+    res.writeHeader(200, {
+        "Content-Type": "text/event-stream"
+        , "Cache-Control": "no-cache"
+        , "Connection": "keep-alive"
+        , "Access-Control-Allow-Origin": "*"
+    });
+    var interval = setInterval(function () {
+        var ret = '';
+        if(msgs.length != 0){
+            for(const msg of msgs){
+                ret += msg;
+            }
+            res.write("data: " + ret + "\n\n");
+        }
+    }, 1000);
+}).listen(9090);
+console.log('SSE-Server started!');
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
 wss = new WebSocketServer({ port: 8080, path: '/testing' });
 users = [];
+msgs = [];
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
-        console.log('Message in: ' + message);
+        // console.log('Message in: ' + message);
         if(ws['name'] == null){
             ws['name'] = message;
             users.push(ws);
             console.log('User: ' + ws['name']);
         }else{
+            var msg = ws['name'] + ': ' + message;
+            msgs.push(msg);
             for(const user of users){
                 if(user['name'] != ws['name']){
-                    user.send(ws['name'] + ': ' + message);
+                    user.send(msg);
                 }
             }
         }
         // console.log('Msg received in server: %s ', message);
     });
-    console.log('new connection');
+    console.log('new connection ws');
     ws.send('Please inform your name!');
 });
